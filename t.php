cff6682a928f832ca3eb9f7a31509773393ad7c0
Roh Geonho <roh517@naver.com>
2019-08-27 20:42:03 +0900

    dd

---
+++ t.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>HTML 사이트 기본 틀</title>
+    </head>
+
+    <body>
+            <h1>테스트 사이트 입니다</h2>
+            
+            <form action = "login_sample.php" method="POST">
+                    <p>아이디</p>
+                    <input type="text" name="id">
+                    <p>비밀번호</p>
+                    <input type="number" name="pw">
+                    <button type="submit" value="계산하기">가입하기</button>
+            </form>
+    </body>
+
+</html> 
