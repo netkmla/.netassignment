dd8d9aaae156ae9d0f9b84116c6fb0a28e75ebab
Roh Geonho <roh517@naver.com>
2019-08-27 20:19:48 +0900

    dd

---
+++ sample.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>HTML 사이트 기본 틀</title>
+    </head>
+
+    <body>
+            <h1>테스트 사이트 입니다</h2>
+            
+            <form action = "action.php" method="POST">
+                    <input type="number" name="num1">
+                    더하기
+                    <input type="number" name="num2">
+                    <button type="submit" value="계산하기">계산하기</button>
+            </form>
+    </body>
+
+</html> 
