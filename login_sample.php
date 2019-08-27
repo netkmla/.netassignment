7aa56846eba2d959b01f6448c64963b3c475ea69
Roh Geonho <roh517@naver.com>
2019-08-27 20:49:18 +0900

    query

--- login_sample.php
+++ login_sample.php
@@ -6,10 +6,9 @@
     $db = substr($url["path"], 1);
     $mysqli=mysqli_connect($server, $username, $password, $db);
     
-    echo $server;
-    echo "<br>";
-    echo $username;
-    echo "<br>";
-    echo $password;
-    echo "<br>";
+    $id = $_POST["id"];
+    $name = $_POST["name"];
+    $pw = $_POST["pw"];
+
+    $q = mysqli_query($mysqli,"INSERT INTO user_data(id,username,userpw) VALUES('$id', '$name', '$pw')");
 ?>
