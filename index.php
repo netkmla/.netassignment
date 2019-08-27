74297577290610e4ca1fcc46165ce4817a1c24a0
Roh Geonho <roh517@naver.com>
2019-08-27 19:51:06 +0900

    3

--- index.php
+++ index.php
@@ -1,98 +1,119 @@
-<!DOCTYPE html>
-
+<!doctype html>
 <html>
+
+<head>
+  <title> 노건호 지원서</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="1.css">
+  <link rel="stylesheet" href="design.css">
   <style>
-
-    <meta charset="utf-8">
-
-    .test{
-      display: flex;
-      flex-direction: row;
-      justify-content: space-around;
-    }
-    .test_1{
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-    }
-    .space{
-      margin-bottom:5px;
-      margin-top:5px;
-    }
-    .result{
-      color:red;
-      border:none;
-      font-size:15px;
-    }
-    .row_view{
-      display : flex;
-      flex-direction: row;
-      justify-content: space-between;
+    h1 {
+      color: #474747;
+      font-size: 60px;
     }
 
+    p.one {
+      line-height: 10px;
+    }
+
+    p.two {
+      line-height: 20px;
+    }
+
+    .margin-center {
+      margin-bottom: 15px;
+      margin-top: 15px;
+    }
+
+    .general-link {
+      font-size: 15px;
+      color: #000000;
+    }
+
+    body {
+      background: rgba(121, 171, 255, 0.3);
+
+    }
   </style>
+</head>
 
+<body>
 
-  <body>
-    <div class="test_1" align="middle">
-      <div class="test space" >
-        첫 번째 수
-        <input type="number" id="first_number" value="">
-      </div>
-      <div class="test space" >
-        연산자
-        <input type="text" id="operator" value="">
-      </div>
-      <div class="test space" >
-        두 번째 수
-        <input type="number" id="second_number" value="">
-      </div>
-      <div class="test space">
-         <button type="button" id="calculate" onclick="output()">계산</button>
-      </div>
-      <div class="test space">
-        결과
-        <input type="text" class="result " id="result" value="">
+  <header>
+    <div id="header" >
+      <div class="main_lnb">
+        <div class="header_sec">
+          <div class="inner_sec">
+            <div class="lnb" id="lnb">
+              <div class="wrap" style="height: 0px; transition: all 0.5s ease 0s;" >
+                <ul>
+                    <li class=""><a href="https://www.facebook.com/profile.php?id=100014804491301&ref=bookmarks" target="_blank">Facebook</a></li>
+                    <li class=""><a href="https://github.com/nickroh" target="_blank">Github</a></li>
+                    <li class=""><a href="https://kmlaonline.net/user/view/2263/roh517" target="_blank">KMLA Online</a></li>
+                </ul>
+              </div>
+          </div>
+        </div>
       </div>
     </div>
- </body>
+  </div>
+  </header>
 
- <script type="text/javascript">
+  <div class="general">
+    <h1 class="table"> .NET APPLICATION </h1>
+    <br>
+    <div> Geonho Roh</div> <br>
+    <div>
+      <table class="type04">
+        <tr>
+          <th>이름</th>
+          <td><a href="https://www.facebook.com/profile.php?id=100014804491301&ref=bookmarks" target="_blank" class="general-link">노건호</a></td>
+        </tr>
+        <tr>
+          <th >학번</th>
+          <td>181040</td>
+        </tr>
+        <tr>
+          <th >기수</th>
+          <td>23기</td>
+        </tr>
+        <tr>
+          <th>Gmail</th>
+          <td><a href="mailto:overwatch517517@gmail.com" target="_blank" class="general-link"> gmail  </a></td>
+        </tr>
+        <tr>
+          <th>Naver Mail</th>
+          <td><a href="mailto:roh517@naver.com" target="_blank" class="general-link"> naver 메일  </a></td>
+        </tr>
+      </table>
+    </div>
 
- var temp;
+    <div class="margin-center general">
+      <h2>
+        닷넷 지원이유
+      </h2>
+      <section class="margin-center passage">
+        닷넷에서 활동을 해보면서 웹분야에 대해 더 공부해보고 싶었고, 많은 학생들이 이용하는 사이트인 kmlaonline을 관리하고 개선하고 싶어 지원하였습니다
+      </section>
+      <h2>
+        컴퓨터와 관계
+      </h2>
+      <section class="margin-center passage">
+        저는 컴퓨터를 좋아합니다, 처음에는 하드웨어(GPU, CPU...)등과 게임에 대한 관심을
+        바탕으로 컴퓨터를 좋아하게 되었습니다. 이러한 관심을 통해 점차 소프트웨어적인 부분에도 관심을 가지게 되었습니다.
+        학교에서 수업을 통해 java를 접하고 나서 C언어를 통해 알고리즘을 공부해보았고, Python을 이용한 머신러닝을 접해보고, AHK 와 같은 언어도 다뤄보고
+        C#을 통해서 계산기 앱을 만들어 보기도 했습니다. 현재는 C#을 기반으로 유니티를 통해 게임개발에 관심을 가지게 되어 게임개발을 해보고 있으며 또한 node.js , ruby등을 공부할 기회가 생겨서
+        공부해 보고 있습니다.<br><br>
+        또한 봉사동아리인 BCD 에서 둔내초등학교 아이들에게 파이썬을 가르쳐주는 봉사활동을 진행하고 있습니다
 
- function output(){
- var first_number = document.getElementById("first_number").value;
- var operator = document.getElementById("operator").value;
- var second_number = document.getElementById("second_number").value;
+      </section>
+    </div>
 
+  </div>
 
-  a=Number(first_number);
-  b=Number(second_number);
-  if(operator == '*')
-  {
-      temp = a * b ;
-      document.getElementById("result").value = temp;
-  }
-  if(operator == '+')
-  {
-    temp = a + b;
-    document.getElementById("result").value = temp;
-  }
-  if(operator == '-')
-  {
-    temp = a-b;
-    document.getElementById("result").value = temp;
-  }
-  if(operator == '/')
-  {
-    temp = a / b;
-    document.getElementById("result").value = temp;
-  }
+</body>
+<div class="margin-center">
 
- }
+</div>
 
-
-
- </script>
 </html>
